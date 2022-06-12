169741dfd39835387b7409b441b542677047b0ab
bondxxl <bondxxl01@gmail.com>
2022-06-12 23:22:45 +0300

    Final validations for users

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,4 +1,9 @@
 const {user} = require('../models/user');
+
+// https://verifalia.com/validate-email
+const gmailRGEX = /^[a-z\d](([.+])?[a-z\d]){5,}@g(oogle)?mail\.com$/;
+const phoneRGEX = /^\+380\d{9}$/;
+
 const createUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during creation
     let keys = Object.keys(req.body);
@@ -8,7 +13,12 @@ const createUserValid = (req, res, next) => {
         req.body
         && keys.length === modelKeys.length - 1
         && !keys.includes('id')
+        && String(req.body.firstName).length > 0
+        && String(req.body.lastName).length > 0
         && keys.every(key => modelKeys.includes(key))
+        && gmailRGEX.test(req.body.email)
+        && phoneRGEX.test(req.body.phoneNumber)
+        && String(req.body.password).length >= 3
     ) {
         next();
     } else {
@@ -25,6 +35,11 @@ const updateUserValid = (req, res, next) => {
         && keys.length
         && !keys.includes('id')
         && keys.every(key => modelKeys.includes(key))
+        && (!req.body.firstName || String(req.body.firstName).length > 0)
+        && (!req.body.lastName || String(req.body.lastName).length > 0)
+        && (!req.body.email || gmailRGEX.test(req.body.email))
+        && (!req.body.phoneNumber || phoneRGEX.test(req.body.phoneNumber))
+        && (!req.body.password || String(req.body.password).length >= 3)
     ) {
         next();
     } else {
