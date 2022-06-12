f279503ca72ac03d4cd0afa739a801913c638d0a
bondxxl <bondxxl01@gmail.com>
2022-06-12 18:24:28 +0300

    Implemented CRUD methods for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,23 +8,35 @@ const router = Router();
 // TODO: Implement route controllers for user
 
 router.get('/', function(req, res, next) {
-
+    let all = UserService.getAll();
+    res.send(`${all.reduce((res, el) => res + el.id + '\n', '')}`);
 });
 
 router.post('/', function(req, res, next) {
-
+//    email, password, firstName, lastName, phoneNumber
+    res.send(UserService.create(req.body).id);
 });
 
 router.get('/:id', function(req, res, next) {
-
+    let user = UserService.getById(req.params.id);
+    if (user) {
+        res.send(`Hi, ${user.id}`);
+    }
 });
 
 router.put('/:id', function(req, res, next) {
-
+    let updated = UserService.update(req.params.id, req.body);
+    if (updated) {
+        res.send(`Updated ${updated.id}`);
+    }
 });
 
 router.delete('/:id', function(req, res, next) {
-
+    let deleted = UserService.delete(req.params.id);
+    console.log(`deleted: `, deleted);
+    if (deleted.length) {
+        res.send(`Deleted ${deleted[0].id}`);
+    }
 });
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,50 @@ class UserService {
 
     // TODO: Implement methods to work with user
 
+    create(data) {
+        const newUser = UserRepository.create(data);
+        return newUser;
+    }
+
+    getAll() {
+        const all = UserRepository.getAll();
+        if(!all) {
+            return null;
+        }
+        // console.log(all);
+        return all;
+    }
+
+    getById(id) {
+        let found = this.search({id: id});
+        return found;
+    }
+
+    update(id, data) {
+        let updated = UserRepository.update(id, data);
+        return updated;
+    }
+
+    delete(id) {
+        let deleted = UserRepository.delete(id);
+        return deleted;
+    }
+
+    getByName(name) {
+        let found = this.search({name: name.toLowerCase()});
+        return found;
+    }
+
+    getByEmail(email) {
+        let found = this.search({email: email});
+        return found;
+    }
+
+    getByPhoneNumber(phoneNumber) {
+        let found = this.search({phoneNumber: phoneNumber});
+        return found;
+    }
+
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
