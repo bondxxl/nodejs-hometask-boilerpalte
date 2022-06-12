ffe832926c9a03a6c48542a35433d30f26284f9f
bondxxl <bondxxl01@gmail.com>
2022-06-12 13:39:44 +0300

    Added empty routes to the userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,24 @@ const router = Router();
 
 // TODO: Implement route controllers for user
 
+router.get('/', function(req, res, next) {
+
+});
+
+router.post('/', function(req, res, next) {
+
+});
+
+router.get('/:id', function(req, res, next) {
+
+});
+
+router.put('/:id', function(req, res, next) {
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+});
+
 module.exports = router;
