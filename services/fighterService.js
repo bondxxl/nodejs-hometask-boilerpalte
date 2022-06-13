5c73901f54f8d15e0debc14ed7da38db22f27219
bondxxl <bondxxl01@gmail.com>
2022-06-14 00:27:37 +0300

    Added methods to fighter service

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,47 @@ const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+
+    create(data) {
+        const newUser = FighterRepository.create(data);
+        return newUser;
+    }
+
+    getAll() {
+        const all = FighterRepository.getAll();
+        if(!all) {
+            return null;
+        }
+        return all;
+    }
+
+    getById(id) {
+        let found = this.search({id: id});
+        return found;
+    }
+
+    update(id, data) {
+        let updated = FighterRepository.update(id, data);
+        return updated;
+    }
+
+    delete(id) {
+        let deleted = FighterRepository.delete(id);
+        return deleted;
+    }
+
+    getByName(name) {
+        let found = this.search({name: name});
+        return found;
+    }
+
+    search(search) {
+        const item = FighterRepository.getOne(search);
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
 }
 
 module.exports = new FighterService();
