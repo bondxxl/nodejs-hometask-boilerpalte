6157a45e36df0d49d2e4d46e999abc7dccb85a6e
bondxxl <bondxxl01@gmail.com>
2022-06-14 00:10:00 +0300

    Added template to fighter routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -7,4 +7,24 @@ const router = Router();
 
 // TODO: Implement route controllers for fighter
 
+router.get('/', function (req, res, next) {
+
+}, responseMiddleware);
+
+router.post('/', createFighterValid, function (req, res, next) {
+
+}, responseMiddleware);
+
+router.get('/:id', function (req, res, next) {
+
+}, responseMiddleware);
+
+router.put('/:id', updateFighterValid, function (req, res, next) {
+
+}, responseMiddleware);
+
+router.delete('/:id', function (req, res, next) {
+
+}, responseMiddleware);
+
 module.exports = router;
