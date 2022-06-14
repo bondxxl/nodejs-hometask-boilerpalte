fc6b618861df7cda84c4dce71ba233383b3fcb19
bondxxl <bondxxl01@gmail.com>
2022-06-14 04:52:55 +0300

    Added error handler to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,22 +7,29 @@ const router = Router();
 
 // TODO: Implement route controllers for user
 
+const errorResponder = (err, req, res, next) => {
+    res.header("Content-Type", 'application/json')
+
+    const status = err.status || 400
+    res.status(status);
+    res.err = err;
+    next();
+}
+
 router.get('/', function (req, res, next) {
     const all = UserService.getAll();
     try {
         if (all) {
             res.data = all;
+            next();
         } else {
             throw Error("Unable to get users");
         }
     } catch (err) {
-        // next(err);
         res.status(404);
-        res.err = err;
-    } finally {
-        next();
+        next(err);
     }
-}, responseMiddleware);
+}, errorResponder, responseMiddleware);
 
 router.post('/', createUserValid, function (req, res, next) {
 //    email, password, firstName, lastName, phoneNumber
@@ -35,67 +42,56 @@ router.post('/', createUserValid, function (req, res, next) {
         const created = UserService.create(req.body);
         if (created) {
             res.data = created;
+            next();
         } else {
             throw Error("Unable to create user");
         }
     } catch (err) {
-        // next(err);
-        res.status(400);
-        res.err = err;
-    } finally {
-        next();
+        next(err);
     }
-}, responseMiddleware);
+}, errorResponder, responseMiddleware);
 
 router.get('/:id', function (req, res, next) {
     const found = UserService.getById(req.params.id);
     try {
         if (found) {
             res.data = found;
+            next();
         } else {
             throw Error("User not found");
         }
     } catch (err) {
-        // next(err);
         res.status(404);
-        res.err = err;
-    } finally {
-        next();
+        next(err);
     }
-}, responseMiddleware);
+}, errorResponder, responseMiddleware);
 
 router.put('/:id', updateUserValid, function (req, res, next) {
     const updated = UserService.update(req.params.id, req.body);
     try {
         if (updated) {
             res.data = updated;
+            next();
         } else {
             throw Error("Unable to update user");
         }
     } catch (err) {
-        // next(err);
-        res.status(400);
-        res.err = err;
-    } finally {
-        next();
+        next(err);
     }
-}, responseMiddleware);
+}, errorResponder, responseMiddleware);
 
 router.delete('/:id', function (req, res, next) {
     const deleted = UserService.delete(req.params.id);
     try {
         if (deleted.length) {
             res.data = deleted;
+            next();
         } else {
             throw Error("Unable to delete user");
         }
     } catch (err) {
-        // next(err);
-        res.status(400);
-        res.err = err;
-    } finally {
-        next();
+        next(err);
     }
-}, responseMiddleware);
+}, errorResponder, responseMiddleware);
 
 module.exports = router;
